0722b415dff86d77ecbc015cc22c94113002befa
Alexey <chuev.04@mail.ru>
2026-03-31 05:34:36 +0700

    Move config file to AppData directory
    Updated the configuration file path to be stored in the AppData directory and ensured the directory is created if it doesn't exist.

--- proxy/hwid_auth.py
+++ proxy/hwid_auth.py
@@ -5,7 +5,10 @@ import os
 
 # Эту строку GitHub автоматически заменит на твой секретный пароль при сборке
 SECRET_SALT = "REPLACE_ME_SECRET_SALT"
-CONFIG_FILE = "license.json"
+
+# Прячем файл лицензии в системную папку AppData/Roaming/TGProxy
+APP_DATA_DIR = os.path.join(os.environ.get('APPDATA', os.path.expanduser('~')), 'TGProxy')
+CONFIG_FILE = os.path.join(APP_DATA_DIR, "license.json")
 
 def get_hwid():
     """Получает серийный номер системного диска Windows"""
@@ -18,10 +21,8 @@ def get_hwid():
 
 def generate_key(hwid):
     """Генерирует уникальный ключ на основе HWID и секретной соли"""
-    # Оставили только базовую проверку на пустоту, чтобы GitHub Actions ничего не сломал
     if not SECRET_SALT:
         return "ERROR_NO_SALT"
-        
     return hashlib.sha256((hwid + SECRET_SALT).encode()).hexdigest()[:12]
 
 def is_activated():
@@ -42,11 +43,14 @@ def is_activated():
         expected_key = generate_key(current_hwid)
         
         return saved_key == expected_key
-        
     except (json.JSONDecodeError, Exception):
         return False
 
 def save_key(key):
     """Сохраняет валидный ключ в JSON"""
+    # Создаем папку TGProxy в AppData, если её еще нет
+    if not os.path.exists(APP_DATA_DIR):
+        os.makedirs(APP_DATA_DIR)
+        
     with open(CONFIG_FILE, 'w') as f:
         json.dump({"key": key}, f)
